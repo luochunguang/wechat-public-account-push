307bcb33d139fadd8b7cf774cc1678ca337de914
luochunguang <53896301+luochunguang@users.noreply.github.com>
2022-10-10 17:26:01 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '忆君',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'olwYF5-0DOzf09ao5PhK_GNWpUg8',
+      id: 'olwYF517vkI53i0OmhyHk2wANk4M',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'RcuugIpc1y6oCi2YgxNHR6Q3gEJl0f41FZ4uJCa2h_M',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
