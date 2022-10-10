3c2aa8f215d08f1103d4b4b9907b775c3d373e87
luochunguang <53896301+luochunguang@users.noreply.github.com>
2022-10-10 15:33:26 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -36,7 +36,7 @@ const USER_CONFIG = {
           type: '生日', name: '忆君', year: '2000', date: '10-20',
         },
         {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+          type: '节日', name: '相识纪念日', year: '2022', date: '02-07',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
