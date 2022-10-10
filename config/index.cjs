3c64683eeb4096f1ed56ae903d490f1ebd458fca
luochunguang <53896301+luochunguang@users.noreply.github.com>
2022-10-10 15:38:47 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -47,11 +47,41 @@ const USER_CONFIG = {
         { keyword: 'marry_day', date: '2022-09-09' },
       ],
     },
+      {
+      // 想要发送的人的名字
+      name: '忆君',
+      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: 'olwYF5-0DOzf09ao5PhK_GNWpUg8',
+      // 使用微信测试号：你想对他发送的模板消息的模板ID
+      useTemplateId: 'VhbwmPbvV1ghfeKn3QCMon9Z_PtJI0O6-Q6E63oL1uk',
+      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
+      horoscopeDate: '10-20',
+      festivals: [
+        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+        {
+          type: '*生日', name: '忆君', year: '2000', date: '09-23',
+        },
+        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
+        {
+          type: '生日', name: '忆君', year: '2000', date: '10-20',
+        },
+        {
+          type: '节日', name: '相识纪念日', year: '2022', date: '02-07',
+        },
+      ],
+      // 我们在一起已经有xxxx天了的配置
+      customizedDateList: [
+        // 在一起的日子
+        { keyword: 'love_day', date: '2022-09-08' },
+        // 结婚纪念日
+        { keyword: 'marry_day', date: '2022-09-09' },
+      ],
+    },
   ],
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'VhbwmPbvV1ghfeKn3QCMon9Z_PtJI0O6-Q6E63oL1uk',
+  CALLBACK_TEMPLATE_ID: '',
 
   CALLBACK_USERS: [
     {
